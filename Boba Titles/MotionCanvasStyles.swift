43c5aeb8a5c15261a471efd6807460e47bbc2a50
Joel <58995538+jtpotato@users.noreply.github.com>
2024-01-23 20:13:43 +1100

    add motion canvas styles

--- Boba Titles/MotionCanvasStyles.swift
+++ Boba Titles/MotionCanvasStyles.swift
@@ -5,4 +5,14 @@
 //  Created by Joel Tan on 23/1/2024.
 //
 
-import Foundation
+var DefaultTextStyle = """
+const textDefaults = {
+  fontSize: 40,
+  fill: "#fff",
+  fontFamily: "monospace",
+  opacity: 0,
+  x: 0,
+  stroke: "#fa0",
+  lineWidth: 2
+}
+"""
