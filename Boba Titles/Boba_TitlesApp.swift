07bdd8e3bb4554192bb408a9564becb14fec32ad
Joel <58995538+jtpotato@users.noreply.github.com>
2024-01-23 15:05:20 +1100

    Initial Commit

---
+++ Boba Titles/Boba_TitlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boba_TitlesApp.swift
+//  Boba Titles
+//
+//  Created by Joel Tan on 23/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct Boba_TitlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boba Titles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Boba Titles
+//
+//  Created by Joel Tan on 23/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
